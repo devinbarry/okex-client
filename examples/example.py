ea766180861ec75f734c99e8574812c6c09aee5e
Devin Barry <devinbarry@gmail.com>
2020-11-11 05:01:44 +0000

    English translation of websocket example.

--- examples/example.py
+++ examples/example.py
@@ -10,10 +10,12 @@ import okex.information_api as information
 import json
 import datetime
 
+
 def get_timestamp():
     now = datetime.datetime.now()
     t = now.isoformat("T", "milliseconds")
     return t + "Z"
+
 
 time = get_timestamp()
 
